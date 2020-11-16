4a1cdd0547a9a2e8d9ea67261be3eb410b0b48f3
demetreus420 <dima.net.dd@gmail.com>
2020-11-17 00:50:50 +0200

    Updates

---
+++ precache-manifest.a28fc17bcf0eb67c4e426d8ea3958ff1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "239ed804f3a8bdf22e86f10e96c830e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53193ab6875577e003bb",
+    "url": "/static/css/main.87b243fd.chunk.css"
+  },
+  {
+    "revision": "516970b4e8e71d3362a6",
+    "url": "/static/js/2.cf5e0c2f.chunk.js"
+  },
+  {
+    "revision": "851820dc40bca3bcaf80cf6dd26069f9",
+    "url": "/static/js/2.cf5e0c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53193ab6875577e003bb",
+    "url": "/static/js/main.75269c7f.chunk.js"
+  },
+  {
+    "revision": "3076b037e6276ad236a1",
+    "url": "/static/js/runtime-main.bf4bdaea.js"
+  }
+]);
