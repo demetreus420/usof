b4eb27c1c860d464200fbec2adc771c189e9aec2
demetreus420 <dima.net.dd@gmail.com>
2020-11-17 11:54:21 +0200

    Updates

--- precache-manifest.4bd354f69ebd04865896c9bfff3fa558.js
+++ precache-manifest.4bd354f69ebd04865896c9bfff3fa558.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "734b4689e852f1eb730b1ef9efa958f6",
+    "revision": "37de93e0330ac779c62a9068226a26bf",
     "url": "/usof/index.html"
   },
   {
-    "revision": "1c6b24e15cda1b91dc0b",
+    "revision": "3231bb8060baaf514fb6",
     "url": "/usof/static/css/main.723cd43c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/usof/static/js/2.cf5e0c2f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1c6b24e15cda1b91dc0b",
-    "url": "/usof/static/js/main.aba9fe90.chunk.js"
+    "revision": "3231bb8060baaf514fb6",
+    "url": "/usof/static/js/main.0eac4eae.chunk.js"
   },
   {
     "revision": "0d588a82f861d39e42bc",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/usof/precache-manifest.c8b7f0fbbdec82b79334fbde7e939873.js"
+  "/usof/precache-manifest.4bd354f69ebd04865896c9bfff3fa558.js"
 );
 
 self.addEventListener('message', (event) => {
